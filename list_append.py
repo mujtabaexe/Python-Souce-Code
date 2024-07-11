366c2e71030b6ccd97c0ed517a03a34f6a79d689
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-11 15:37:37 +0500

    list functions

---
+++ list_append.py
@@ -0,0 +1,59 @@
+# names= []
+# while True:
+#     add=input("Enter the names you want to store or enter exit to exit: ")
+#     if add=="exit":
+#         break
+#     names.append(add)
+    
+# print(names)
+    
+
+
+
+
+
+# l=[23,1,2,3,4,5,44,3,2,55,34]
+# l.reverse()
+# print(l)
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+
+
+# l=[23,55,999,1,2,3,4,5,44,3,2,55,34]
+# print(l,"Printing l \n")
+# c=l.copy()
+# c[0]=0
+# print(c,"Printing c")
+# print(l,"Printing l in the c space\n")
+# m=l
+# m[0]=0
+# print(l,"Printing l in the m space")
+# print(m,"Printing m")
+# print(l,"Printing l in the m space")
+
+
+
+l=[23,1,2,3,4,5,44,3,2,55,34]
+l.insert(10, 999)
+print(l)
+n=["Sahibzada","Ahmad","Mujtaba"]
+k=n+l
+print(k,"\n")
+print(l)
+k=l+n
+print(k)
+print(l,"\n")
+m=["Sahibzada","Ahmad","Mujtaba"]
+l.extend(m)
+print(l,"Extended")
+l.insert(1, 999999)
+print(l)
+
+
+
+
+
+
+
