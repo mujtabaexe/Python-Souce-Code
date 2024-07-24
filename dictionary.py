96a9da708a9f7d096757da5496ddb7c8495af366
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-24 17:00:14 +0500

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -9,7 +9,14 @@ print(dic.get("emp2)"),"\n") # It will print none
 # print(dic["emp2"])# It will throw an error
 print(dic["emp"],"\n") 
 
-print(dic.keys(),"\n")
+print(dic.keys(),"\n") # Prints the keys 
 
 for key in dic.keys():
-    print(dic[key])
+    print(dic[key]) # Print the values of the keys                   
+print()
+for key in dic.keys():
+    print(f"The value corresponding to the key {key} is {dic[key]}")
+print()
+for key in dic.keys():
+    print(f"The value corresponding to the key {key} is {dic.keys()}") # Just to check 
+print()
