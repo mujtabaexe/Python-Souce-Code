0b8afd519a1a31549e919eda7adc58f5a4b2cbd1
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-24 16:47:41 +0500

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -5,6 +5,11 @@ dic={ 0 : "Sahibzada",
 print(dic[0])
 print(dic[1])
 print(dic[2])
-print(dic.get("emp2)")) # It will print none
-print(dic["emp2"])# It will throw an error
-print(dic["emp"]) 
+print(dic.get("emp2)"),"\n") # It will print none
+# print(dic["emp2"])# It will throw an error
+print(dic["emp"],"\n") 
+
+print(dic.keys(),"\n")
+
+for key in dic.keys():
+    print(dic[key])
