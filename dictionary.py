7ea5458182ab0d52f2aecafa8ac8639b6de7923e
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-24 16:42:05 +0500

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+dic={ 0 : "Sahibzada",
+     1: "Ahmad",
+     2:"Mujtaba",
+     "emp": "emp_Zeeshan"}
+print(dic[0])
+print(dic[1])
+print(dic[2])
+print(dic.get("emp2)")) # It will print none
+print(dic["emp2"])# It will throw an error
+print(dic["emp"]) 
