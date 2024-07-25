029584b33bf4b083dc1896ca19e389732024f036
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-25 18:52:47 +0500

    .

---
+++ file_IO.py
@@ -0,0 +1,8 @@
+# f=open("temp2.IO.txt","w") # makes a new file 
+f = open("temp_IO.txt","a") # "r" mode is also a default
+# print(f) #it prints the info of the text file not the inside mateerial
+# text= f.read()
+# print(text) # Now it will print its inside text also with the help of next line
+# f.close
+
+
