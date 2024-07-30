ff6b2b074dff124653f5cd105f517d7c023b46e0
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-30 18:08:50 +0500

    FIle IO Method

--- file_IO.py
+++ file_IO.py
@@ -6,3 +6,8 @@ f = open("temp_IO.txt","a") # "r" mode is also a default
 # f.close
 
 
+f = open ("my.txt","w")
+lines =["line1","line2","Line3"]
+for line in lines:
+    f.writelines(line+"\n")
+f.close()
