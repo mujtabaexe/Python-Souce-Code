473b9a0f5c12e60c6dc0916f4c65a83e3f1c6fd1
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-07-31 20:20:21 +0500

    constructors

--- constructors.py
+++ constructors.py
@@ -1,26 +1,43 @@
 ### NOW A MORE BETTER WAY TO MAKE CLASS OTHER THAN CLASS AND OBJECT FILE METHOD IS  ...  USING __init__() --> DUNDER METHOD
-    
+
+### JUST BASIC 
 class Person:
     def __init__(self, name,occ):
-        self.name=[]
-        self.occ=[]
-        while True:
-            n=input("Enter Name or \"exit\" to exit: ")
-            if n.lower()==("exit"):
-                break
-            o=input("Enter occupation: ")
-            self.name.append(n)
-            self.occ.append(o)
+        self.name=name
+        self.occ=occ
+        
         
     def info(self):
-        for n, o in zip(self.name, self.occ):    
-            print(f"{n} is a {o}")
-        
+        print(f"{self.name} is a {self.occ}")
+
+
 a=Person("Mujtaba", "Developer")
 a.info()
 
 
+### CREATED A PROGRAM
 
+# class Person:
+#     def __init__(self, name,occ):
+#         self.name=[]
+#         self.occ=[]
+#         while True:
+#             n=input("Enter Name or \"exit\" to exit: ")
+#             if n.lower()==("exit"):
+#                 break
+#             o=input("Enter occupation: ")
+#             self.name.append(n)
+#             self.occ.append(o)
+        
+#     def info(self):
+#         for n, o in zip(self.name, self.occ):    
+#             print(f"{n} is a {o}")
+        
+# a=Person("Mujtaba", "Developer")
+# a.info()
+
+
+### GPT PROGRAM
 
 # class Person:
 #     def __init__(self):
